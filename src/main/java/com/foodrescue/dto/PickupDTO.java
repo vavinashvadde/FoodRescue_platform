47eeaed321e622c8a2bb54c3eb166ee965bc1895
Avinashvadde <avinashvadde68@gmail.com>
2026-08-15 00:10:14 +0530

    Remove Lombok annotations from PickupDTO
    Removed Lombok annotations from PickupDTO class.

--- src/main/java/com/foodrescue/dto/PickupDTO.java
+++ src/main/java/com/foodrescue/dto/PickupDTO.java
@@ -5,10 +5,6 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class PickupDTO {
 
     private Long pickupId;
